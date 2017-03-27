fb219d9d77f3e3f55f1c6b0d01e7d1d1241ebca6
Horace Williams <horace.d.williams@gmail.com>
2017-03-26 21:14:54 -0700

    add puma, procfile, and rack config for heroku

--- app.rb
+++ app.rb
@@ -1,26 +1,28 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
 require 'pg'
 
 DB = PG.connect(dbname: 'foodoasis_gis')
 
-def nearby_locations(lat, lon, radius = 3500)
-  lat = lat.to_f
-  lon = lon.to_f
-  radius = radius.to_i
-  query = "SELECT * FROM locations WHERE ST_DWithin(point, ST_Point(#{lon}::numeric, #{lat}::numeric), #{radius}::numeric);"
-  DB.exec(query)
-end
+class FoodOasis < Sinatra::Base
+  def nearby_locations(lat, lon, radius = 3500)
+    lat = lat.to_f
+    lon = lon.to_f
+    radius = radius.to_i
+    query = "SELECT * FROM locations WHERE ST_DWithin(point, ST_Point(#{lon}::numeric, #{lat}::numeric), #{radius}::numeric);"
+    DB.exec(query)
+  end
 
-get '/health' do
-  content_type :json
-  {status: "OK"}.to_json
-end
+  get '/health' do
+    content_type :json
+    {status: "OK"}.to_json
+  end
 
-get '/nearby' do
-  content_type :json
-  results = nearby_locations(params[:latitude], params[:longitude])
-  results.group_by do |r|
-    r['location_type']
-  end.to_json
+  get '/nearby' do
+    content_type :json
+    results = nearby_locations(params[:latitude], params[:longitude])
+    results.group_by do |r|
+      r['location_type']
+    end.to_json
+  end
 end
